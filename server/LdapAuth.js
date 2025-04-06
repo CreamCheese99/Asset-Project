e3ee28c26d39124dd7ac52dbd8af8cbeb61f0314
khemmasedth <khmasade555@gmail.com>
2025-04-06 11:14:23 +0700

    update ldap

--- server/LdapAuth.js
+++ server/LdapAuth.js
@@ -1,177 +1,3 @@
-// const ldap = require('ldapjs');
-
-// class LdapAuth {
-//   constructor(host, port, baseDn) {
-//     this.ldapHost = host;
-//     this.ldapPort = port;
-//     this.ldapBaseDn = baseDn;
-//     this.ldapClient = null;
-//   }
-
-//   // Connect to LDAP server with anonymous bind
-  
-
-//   async connect() {
-//    // console.log("befor connnect");
-//     if (this.ldapClient === null) {
-//       return new Promise((resolve, reject) => {
-//         console.log("initial variable to connect");
-//         console.log("Url="+`ldap://${this.ldapHost}:${this.ldapPort}`)
-//         const client = ldap.createClient({
-//           url: `ldap://${this.ldapHost}:${this.ldapPort}`,
-//           timeout: 5000,
-//           connectTimeout: 10000
-//         });
-
-//         client.on('error', (err) => {
-//           reject(new Error('Failed to connect to LDAP server: ' + err.message));
-//         });
-
-//         // Attempt anonymous bind
-//         client.bind('', '', (err) => {
-//           if (err) {
-//             reject(new Error('Cannot bind to LDAP server anonymously: ' + err.message));
-//           } else {
-//             this.ldapClient = client;
-//             resolve();
-//           }
-//         });
-//       });
-//     }
-//   }
-
-//   // Authenticate user
-//   async authenticate(username, password) {
-//     try {
-//       await this.connect();
-
-//       return new Promise((resolve, reject) => {
-//         const searchOptions = {
-//           scope: 'sub',
-//           filter: `(mail=${username})`,
-//         };
-
-      
-//         this.ldapClient.search(this.ldapBaseDn, searchOptions, (err, res) => {
-//           if (err) {
-//             reject(new Error('LDAP search failed: ' + err.message));
-//             return;
-//           } 
-
-//           let found = false;
-
-//           res.on('searchEntry', (entry) => {
-//             found = true;
-//             const userDn = entry.objectName;
-
-//             //console.log("userDn=".userDn);
-//             const user = entry.object;
-           
-
-//             // Try to bind with user credentials
-//             const client = ldap.createClient({
-//               url: `ldap://${this.ldapHost}:${this.ldapPort}`
-//             });
-//             //console.log("befor bind LDAP, userDn="+userDn+" password="+password);
-
-//             client.bind(String(userDn), String(password), (err) => {
-
-//               if (err) {
-//                 switch(err.code) {
-//                     case 49: // Invalid credentials
-//                         console.log("ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง");
-//                         break;
-//                     case 32: // No such object
-//                         console.log("ไม่พบบัญชีผู้ใช้");
-//                         break;
-//                     case 53: // Account disabled/expired
-//                         console.log("บัญชีผู้ใช้ถูกปิดการใช้งานหรือหมดอายุ");
-//                         break;
-//                     default:
-//                         console.log("เกิดข้อผิดพลาดในการ authenticate:", err.message);
-//                 }
-//                 reject(new Error('Authentication failed'));
-//                 return;
-//             }
-//             // ถ้าสำเร็จ
-//             resolve(true);
-//             });
-//           });
-
-//           res.on('error', (err) => {
-//             reject(new Error('LDAP search error: ' + err.message));
-//           });
-
-//           res.on('end', (result) => {
-//             if (!found) {
-//               reject(new Error('User not found'));
-//             }
-//           });
-//         });
-//       });
-//     } catch (error) {
-//       // You can add error logging here
-//       // console.error('Authentication error:', error);
-//       return false;
-//     }
-//   }
-
-//   // Get user information
-//   async getUserInfo(username) {
-//     try {
-//       await this.connect();
-
-//       return new Promise((resolve, reject) => {
-//         const searchOptions = {
-//           scope: 'sub',
-//           filter: `(mail=${username})`,
-//         };
-
-//         this.ldapClient.search(this.ldapBaseDn, searchOptions, (err, res) => {
-//           if (err) {
-//             reject(new Error('LDAP search failed: ' + err.message));
-//             return;
-//           }
-
-//           let userData = null;
-
-//           res.on('searchEntry', (entry) => {
-//             userData = entry.object;
-//           });
-
-//           res.on('error', (err) => {
-//             reject(new Error('LDAP search error: ' + err.message));
-//           });
-
-//           res.on('end', () => {
-//             resolve(userData);
-//           });
-//         });
-//       });
-//     } catch (error) {
-//       // You can add error logging here
-//       // console.error('Get user info error:', error);
-//       return null;
-//     }
-//   }
-
-//   // Close LDAP connection
-//   close() {
-//     if (this.ldapClient) {
-//       this.ldapClient.unbind();
-//       this.ldapClient.destroy();
-//       this.ldapClient = null;
-//     }
-//   }
-// }
-
-// module.exports = LdapAuth;
-
-
-
-
-
-
 const ldap = require('ldapjs');
 
 class LdapAuth {
@@ -183,17 +9,12 @@ class LdapAuth {
   }
 
   // Connect to LDAP server with anonymous bind
-  
-
   async connect() {
-   // console.log("befor connnect");
     if (this.ldapClient === null) {
       return new Promise((resolve, reject) => {
-        console.log("initial variable to connect");
-        console.log("Url="+`ldap://${this.ldapHost}:${this.ldapPort}`)
         const client = ldap.createClient({
           url: `ldap://${this.ldapHost}:${this.ldapPort}`,
-          timeout: 5000,
+          timeout: 5010,
           connectTimeout: 10000
         });
 
@@ -201,7 +22,6 @@ class LdapAuth {
           reject(new Error('Failed to connect to LDAP server: ' + err.message));
         });
 
-        // Attempt anonymous bind
         client.bind('', '', (err) => {
           if (err) {
             reject(new Error('Cannot bind to LDAP server anonymously: ' + err.message));
@@ -214,7 +34,7 @@ class LdapAuth {
     }
   }
 
-  // Authenticate user
+  // Authenticate user and return user info
   async authenticate(username, password) {
     try {
       await this.connect();
@@ -222,53 +42,47 @@ class LdapAuth {
       return new Promise((resolve, reject) => {
         const searchOptions = {
           scope: 'sub',
-          filter: `(mail=${username})`,
+          filter: `(mail=${username})`, // You can change to (uid=${username}) or (sAMAccountName=${username}) as needed
         };
 
-      
         this.ldapClient.search(this.ldapBaseDn, searchOptions, (err, res) => {
           if (err) {
             reject(new Error('LDAP search failed: ' + err.message));
             return;
-          } 
+          }
 
           let found = false;
 
           res.on('searchEntry', (entry) => {
             found = true;
             const userDn = entry.objectName;
-
-            //console.log("userDn=".userDn);
             const user = entry.object;
-           
 
-            // Try to bind with user credentials
             const client = ldap.createClient({
               url: `ldap://${this.ldapHost}:${this.ldapPort}`
             });
-            //console.log("befor bind LDAP, userDn="+userDn+" password="+password);
 
             client.bind(String(userDn), String(password), (err) => {
-
               if (err) {
-                switch(err.code) {
-                    case 49: // Invalid credentials
-                        console.log("ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง");
-                        break;
-                    case 32: // No such object
-                        console.log("ไม่พบบัญชีผู้ใช้");
-                        break;
-                    case 53: // Account disabled/expired
-                        console.log("บัญชีผู้ใช้ถูกปิดการใช้งานหรือหมดอายุ");
-                        break;
-                    default:
-                        console.log("เกิดข้อผิดพลาดในการ authenticate:", err.message);
+                switch (err.code) {
+                  case 49:
+                    console.log("ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง");
+                    break;
+                  case 32:
+                    console.log("ไม่พบบัญชีผู้ใช้");
+                    break;
+                  case 53:
+                    console.log("บัญชีผู้ใช้ถูกปิดการใช้งานหรือหมดอายุ");
+                    break;
+                  default:
+                    console.log("เกิดข้อผิดพลาดในการ authenticate:", err.message);
                 }
                 reject(new Error('Authentication failed'));
                 return;
-            }
-            // ถ้าสำเร็จ
-            resolve(true);
+              }
+
+              // Successful login, return user object
+              resolve(user);
             });
           });
 
@@ -284,13 +98,11 @@ class LdapAuth {
         });
       });
     } catch (error) {
-      // You can add error logging here
-      // console.error('Authentication error:', error);
       return false;
     }
   }
 
-  // Get user information
+  // Get user information by email
   async getUserInfo(username) {
     try {
       await this.connect();
@@ -323,8 +135,6 @@ class LdapAuth {
         });
       });
     } catch (error) {
-      // You can add error logging here
-      // console.error('Get user info error:', error);
       return null;
     }
   }
