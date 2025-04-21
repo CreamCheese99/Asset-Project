b49018e02f02ff04d64459d5e199f52c6f91b644
Creamcheese <thanchira50045@gmail.com>
2025-04-21 22:33:56 +0700

    ปิด รหัสภาควิชา

--- client/src/components/DataTable.jsx
+++ client/src/components/DataTable.jsx
@@ -611,7 +611,7 @@ const exportPDFAllRow = () => {
             <tr className="bg-gray-200 text-gray-700">
               <th className="border px-4 py-2">รหัสทรัพย์สิน</th>
               <th className="border px-4 py-2 hidden lg:table-cell">ชื่อทรัพย์สิน</th>
-              <th className="border px-4 py-2">รหัสภาควิชา</th>
+             {/* <th className="border px-4 py-2">รหัสภาควิชา</th> */}
               <th className="border px-4 py-2">ภาควิชา</th>
               <th className="border px-4 py-2">ประเภทสินทรัพย์</th>
               <th className="border px-4 py-2">ผู้รับผิดชอบ</th>
@@ -641,7 +641,7 @@ const exportPDFAllRow = () => {
                 <tr key={item.main_asset_id} className="text-center">
                   <td className="border px-4 py-2">{item.main_asset_id}</td>
                   <td className="border px-4 py-2 hidden lg:table-cell">{item.main_asset_name}</td>
-                  <td className="border px-4 py-2">{item.department_id || "-"}</td>
+                  {/* <td className="border px-4 py-2">{item.department_id || "-"}</td> */}
                   <td className="border px-4 py-2">{item.department_name || "-"}</td>
                   <td className="border px-4 py-2">{item.asset_type || "-"}</td>
                   <td className="border px-4 py-2">{item.responsible_person || "-"}</td>
