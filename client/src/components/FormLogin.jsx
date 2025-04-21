70ac35e952da61814ae5dfc58f9f8570dcd4d375
khemmasedth <khmasade555@gmail.com>
2025-04-21 15:58:09 +0700

    header

--- client/src/components/FormLogin.jsx
+++ client/src/components/FormLogin.jsx
@@ -1,24 +1,3 @@
-// import React from 'react'
-// import FormInputField from './FormInputField'
-// import LogoSection from './LogoSection'
-
-// function FormLogin() {
-//   return (
-//     <div className="w-full space-y-4 max-w-lg px-[60px] py-[54px] bg-white rounded-3xl shadow-inner flex flex-col items-center">
-//     <LogoSection />
-//     <h1>เข้าสู่ระบบ</h1>
-//     <div className="w-96 space-y-8 text-left">
-
-//       <FormInputField label="Username" id="username" type="text" placeholder="Enter your username" />
-//       <FormInputField label="Password" id="password" type="password" placeholder="Enter your password" />
-//       <button style={{ background: '#8bc34a'}} className="w-full py-2  text-white text-base font-medium rounded-lg hover:bg-gray-300 transition duration-200">
-//         Login
-//       </button>
-//     </div>
-//   </div>
-//   )
-// }
-
 // export default FormLogin;
 import React, { useState } from 'react';
 import axios from 'axios';  // Import axios
@@ -59,7 +38,7 @@ function FormLogin() {
       if (response.status === 200 && data.success && data.user) {
         // ตรวจสอบว่ามี data.user ก่อนใช้งาน
         console.log('User data from login response:', data.user);  // เพิ่มการ log ข้อมูลที่ได้จาก API
-        localStorage.setItem('userName', data.user.cn || 'Guest');
+        localStorage.setItem('user_name', data.user.cn || 'Guest');
         localStorage.setItem('roleId', 1); // สมมุติ role ชั่วคราว หรือแยกมาจาก LDAP ถ้ามี
         window.location.href = '/home';
       } else {
